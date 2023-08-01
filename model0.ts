e7818ab1757ab07310cab128092019384ed945c0
calcuis <113646141+calcuis@users.noreply.github.com>
2023-08-01 09:12:26 +0800

    Create model0.ts

---
+++ model0.ts
@@ -0,0 +1,4 @@
+{item.a&&<li><input type="radio" value="a" name="choice" /> {item.a}{values.choice==item.ans && item.ans=="a" &&"✔️"}</li>}
+{item.b&&<li><input type="radio" value="b" name="choice" /> {item.b}{values.choice==item.ans && item.ans=="b" &&"✔️"}</li>}
+{item.c&&<li><input type="radio" value="c" name="choice" /> {item.c}{values.choice==item.ans && item.ans=="c" &&"✔️"}</li>}
+{item.d&&<li><input type="radio" value="d" name="choice" /> {item.d}{values.choice==item.ans && item.ans=="d" &&"✔️"}</li>}
